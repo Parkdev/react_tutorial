70cc497ddd1a6af67608cea61a808885372e0925
Jeeho Park <jpk@ip-192-168-70-30.ap-northeast-2.compute.internal>
2022-06-23 11:11:47 +0900

    constructor to state

--- component/src/Counter.js
+++ component/src/Counter.js
@@ -1,18 +1,25 @@
 import { Component } from "react";
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    // state의 초깃값 설정하기
-    this.state = {
-      number: 0,
-    };
-  }
+  //   constructor(props) {
+  //     super(props);
+  //     // state의 초깃값 설정하기
+  //     this.state = {
+  //       number: 0,
+  //       fixedNumber: 0,
+  //     };
+  //   }
+  //또다른 방식으로 초깃값 설정하기
+  state = {
+    number: 0,
+    fixedNumber: 0,
+  };
   render() {
-    const { number } = this.state;
+    const { number, fixedNumber } = this.state;
     return (
       <div>
         <h1>{number}</h1>
+        <h2>바뀌지 않는 값: {fixedNumber}</h2>
         <button
           // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정
           onClick={() => {
