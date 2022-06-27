60b133e164fee8b9982925630112d54b2a0dc9d7
Jeeho Park <jpk@ip-192-168-70-30.ap-northeast-2.compute.internal>
2022-06-27 10:40:49 +0900

    onKeyPress event

--- component/src/EventPractice.js
+++ component/src/EventPractice.js
@@ -133,6 +133,13 @@ class EventPractice extends Component {
     });
   };
 
+  //onKeyPress 이벤트
+  handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      this.handleClick();
+    }
+  };
+
   render() {
     return (
       <div>
@@ -150,6 +157,7 @@ class EventPractice extends Component {
           placeholder="아무거나 입력해 보세요"
           value={this.state.message}
           onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
         />
         <button onClick={this.handleClick}>확인</button>
       </div>
