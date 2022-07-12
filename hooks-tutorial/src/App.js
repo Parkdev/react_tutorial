edb1a6114046562d0d49fab99aaf05ddefc56785
devpark <devpark29@gmail.com>
2022-07-12 16:10:05 +0900

    set visible

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -6,10 +6,24 @@
 
 // export default App;
 
+import { useState } from "react";
 import Info from "./info";
 
 const App = () => {
-  return <Info />;
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? "숨기기" : "보이기"}
+      </button>
+      <hr />
+      {visible && <Info />}
+    </div>
+  );
 };
 
 export default App;
