0d9b3f7d8b44eabd0c5b38b53b09aa28214d23a5
devpark <devpark29@gmail.com>
2022-07-12 16:02:25 +0900

    시점별 useEffect

--- hooks-tutorial/src/info.js
+++ hooks-tutorial/src/info.js
@@ -1,9 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Info = () => {
   const [name, setName] = useState("");
   const [nickname, setNickname] = useState("");
 
+  // 수정시(렌더링시)에 계속
+  //   useEffect(() => {
+  //     console.log("렌더링이 완료되었습니다!");
+  //     console.log({
+  //       name,
+  //       nickname,
+  //     });
+  //   });
+
+  // 마운트 될때만
+  //   useEffect(() => {
+  //     console.log("마운트 될때만 실행됩니다.");
+  //   }, []);
+
+  // 특정값이 업데이트 될때만
+  useEffect(() => {
+    console.log(name);
+  }, [name]);
+
+  // 뒷정리 (언마운트 되기전에 어떤 작업 수행 하고싶으면)
+  useEffect(() => {
+    console.log("effect");
+    console.log(name);
+    return () => {
+      console.log("cleanup");
+      console.log(name);
+    };
+  }, [name]);
   const onChangeName = (e) => {
     setName(e.target.value);
   };
