2805c66f4797d9e7135ab200c26c678802f81668
devpark <devpark29@gmail.com>
2022-07-12 15:39:23 +0900

    함수 useState

---
+++ hooks-tutorial/src/App.js
@@ -0,0 +1,15 @@
+// import Counter from "./Counter";
+
+// const App = () => {
+//   return <Counter />;
+// };
+
+// export default App;
+
+import Info from "./info";
+
+const App = () => {
+  return <Info />;
+};
+
+export default App;
